458ee1801e28d8668e0c5182d910479ac24a9506
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-07-01 06:45:19 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  root to: 'homes#top'
+  get '/' => 'homes#top'
+  get '/books' => 'books#index'
+  get 'books/index'
+  post '/books' => 'books#create'
+  get 'books/show'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  # resources :books
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
